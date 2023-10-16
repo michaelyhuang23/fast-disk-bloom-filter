e8e19f0c0f9d6de8f27ca99f786e16d20901c4dd
Michael Huang <michaelyhuang23@gmail.com>
2023-10-15 22:37:43 -0400

    accuracy bloom

---
+++ test/accuracy_bloom.cpp
@@ -0,0 +1,60 @@
+#include "bloom.h"
+#include "argh.h"
+#include<iostream>
+#include <math.h>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    argh::parser cmdl(argv);
+    uint64_t n;
+    uint32_t block_size;
+    double error_order;
+    cmdl("n", 10000) >> n; 
+    cmdl("error_order", -2) >> error_order;
+    cmdl("block_size", 4096) >> block_size;
+
+    uint64_t m = (uint64_t)(-error_order * n * 5);
+
+
+    std::cout<< "total bloom filter size: " << m << std::endl;
+    std::cout<< "block size: " << block_size << std::endl;
+    std::cout<< "block count: " << m / (block_size * 8) + 1 << std::endl;
+
+    Bloom* bloom; 
+    bloom = new Bloom("data/accuracy_bloom.bin", "data/accuracy_bloom.meta", m, n, block_size, 1LL<<32LL);
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    uint64_t trial = n;
+    unordered_set<uint64_t> s;
+    for (uint64_t i = 0; i < trial; i++){
+        uint64_t x = rand() % (trial);
+        bloom->add(to_string(x));
+        s.insert(x);
+    }
+    bloom->flush();
+    
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "Time taken to add " << trial << " elements: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms" << std::endl;
+    
+
+    auto t3 = std::chrono::high_resolution_clock::now();
+    uint64_t w_count = 0;
+    for (uint64_t i = 0; i < trial; i++){
+        uint64_t x = rand() % (trial);
+        bool p_contain = bloom->contains(to_string(x));
+        if(p_contain != (s.find(x) != s.end())){
+            w_count++;
+        }
+    }
+    auto t4 = std::chrono::high_resolution_clock::now();
+    std::cout << "Time taken to check " << trial << " elements: " << std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count() << "ms" << std::endl;
+    std::cout << "Percent of mistakes: " << (double)w_count / trial << std::endl;
+
+
+    delete bloom;
+}
